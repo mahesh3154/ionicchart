e2505eb6390d44675a5ed44afb3254f63b6b7076
SLS\mahesh.tadepally <mahesh.tadepally@sharecare.com>
2018-09-04 19:37:44 +0530

    updated example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,8 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { ChartModule } from 'angular2-highcharts';
+import * as highcharts from 'Highcharts';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    ChartModule.forRoot(highcharts)
+
   ],
   bootstrap: [IonicApp],
   entryComponents: [

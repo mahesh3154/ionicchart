843cb8239307640c7bba1d5946e50aece31e3e01
mahesh3154 <mahesh3154@gmail.com>
2018-09-04 20:07:47 +0530

    Add files via upload
    updated changes

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -8,7 +8,33 @@ import { NavController } from 'ionic-angular';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-
+    this.chartOptions = {
+      chart: {
+       type: 'bar'
+      },
+      title: {
+       text: 'Fruit Consumption',
+       style:   {
+         color : "#ff0000"
+       }
+      },
+     xAxis: {
+      categories: ['Apples', 'Bananas', 'Oranges']
+     },
+     yAxis: {
+      title: {
+       text: 'Fruit eaten' 
+      }
+     },
+     series: [{
+      name: 'Jane',
+      data: [1, 0, 4]
+     }, { 
+      name: 'John',
+      data: [5, 7, 3]
+     }]
+     } 
+    
   }
 
 }
